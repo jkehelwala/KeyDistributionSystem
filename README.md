5876c789444d45ff5994ae1ed723454d719b7dd6
jkehelwala <git@jkehelwala.com>
2018-10-04 22:33:35 +0530

    DB update

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
 # KeyDistributionSystem
 
-1. Import db/keydist.sql.
-2. Set up a virtual host in apache. Can do by editing following paths 
+## Setup Notes
 
-For windows and XAMPP, this can be done by editing
-hosts -> C:\Windows\System32\drivers\etc
-httpd-vhosts.conf -> C:\xampp\apache\conf\extra
+1. Import db/keydist.sql.
+2. Set up a virtual host in apache. 
+    
+    1. For windows and XAMPP
+        1. Add following code at end of files.
+            * hosts -> C:\Windows\System32\drivers\etc
+        
+                    127.0.0.1       localhost
+                    127.0.0.1       testsite.com
+
+            * httpd-vhosts.conf -> <Location>\xampp\apache\conf\extra
+            
+                    <VirtualHost *:80>
+                        ServerName localhost
+                        DocumentRoot "C:/xampp/htdocs"
+                    </VirtualHost>
+                    <VirtualHost *:80>
+                        ServerName testsite.com
+                        ServerAlias testsite.com
+                        DocumentRoot "C:/xampp/htdocs/testsite"
+                      <Directory "C:\xampp\htdocs\testsite">
+                        Order allow,deny
+                        Allow from all
+                      </Directory>
+                    </VirtualHost>
 	
+	1. For Linux
+	    * https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-ubuntu-14-04-lts
+
 <hr>
 
-Can register new users (for the time being) through following command
-www.testsite.test/scripts/register.php?uname=admin1&role=0&pass=admin
+## Coding Notes
 
-For role numbers, check class/UserRole.php.
+### Temporary Hooks
+
+* Can register new users (for the time being) through following link
+    * www.testsite.test/scripts/register.php?uname=admin1&role=0&pass=admin
+* For user role numbers, check class/UserRole.php.
+
+### Class structure
+
+// TODO
--- db/keydist.sql
+++ db/keydist.sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2018 at 10:39 AM
+-- Generation Time: Oct 04, 2018 at 06:48 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -21,6 +22,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `keydist`
 --
+CREATE DATABASE IF NOT EXISTS `keydist` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `keydist`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +31,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `accounts`
 --
 
-CREATE TABLE `accounts` (
-  `u_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `u_id` int(11) NOT NULL AUTO_INCREMENT,
   `u_name` varchar(30) NOT NULL,
   `u_role` tinyint(1) NOT NULL,
-  `u_pass` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `u_pass` varchar(64) NOT NULL,
+  PRIMARY KEY (`u_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`u_id`, `u_name`, `u_role`, `u_pass`) VALUES
+(1, 'admin1', 0, '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+(2, 'machine1', 2, 'bc020a35b7f9cb1382e7b534c68e3c531d849b119bf14f75ddead6cc45c3ccc1'),
+(3, 'sysadmin1', 1, 'd577adc54e95f42f15de2e7c134669888b7d6fb74df97bd62cb4f5b73c281db4');
 
 -- --------------------------------------------------------
 
@@ -41,9 +54,11 @@ CREATE TABLE `accounts` (
 -- Table structure for table `key_list`
 --
 
-CREATE TABLE `key_list` (
+CREATE TABLE IF NOT EXISTS `key_list` (
   `r_id` int(11) NOT NULL,
-  `key` text NOT NULL
+  `key` text NOT NULL,
+  `self_notes` varchar(300) NOT NULL,
+  PRIMARY KEY (`r_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -52,10 +67,19 @@ CREATE TABLE `key_list` (
 -- Table structure for table `machines`
 --
 
-CREATE TABLE `machines` (
-  `m_id` int(11) NOT NULL,
-  `m_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `machines` (
+  `m_id` int(11) NOT NULL AUTO_INCREMENT,
+  `m_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`m_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `machines`
+--
+
+INSERT INTO `machines` (`m_id`, `m_name`) VALUES
+(1, 'DockerVM'),
+(2, 'ProductionDB');
 
 -- --------------------------------------------------------
 
@@ -63,74 +87,32 @@ CREATE TABLE `machines` (
 -- Table structure for table `requests`
 --
 
-CREATE TABLE `requests` (
-  `r_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `requests` (
+  `r_id` int(11) NOT NULL AUTO_INCREMENT,
   `u_id` int(11) NOT NULL,
   `m_id` int(11) NOT NULL,
   `key_type` set('UserAccount','NewPublicKey','GuestPublicKey') NOT NULL,
   `admin_u_id` int(11) NOT NULL,
   `admin_approved` tinyint(1) NOT NULL,
-  `key_issued` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
-ALTER TABLE `accounts`
-  ADD PRIMARY KEY (`u_id`);
-
---
--- Indexes for table `key_list`
---
-ALTER TABLE `key_list`
-  ADD PRIMARY KEY (`r_id`);
-
---
--- Indexes for table `machines`
---
-ALTER TABLE `machines`
-  ADD PRIMARY KEY (`m_id`);
-
---
--- Indexes for table `requests`
---
-ALTER TABLE `requests`
-  ADD PRIMARY KEY (`r_id`),
-  ADD KEY `u_id` (`u_id`),
-  ADD KEY `admin_u_id` (`admin_u_id`),
-  ADD KEY `m_id` (`m_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
-ALTER TABLE `accounts`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  `key_issued` tinyint(1) NOT NULL,
+  PRIMARY KEY (`r_id`),
+  KEY `u_id` (`u_id`),
+  KEY `admin_u_id` (`admin_u_id`),
+  KEY `m_id` (`m_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `key_list`
---
-ALTER TABLE `key_list`
-  ADD CONSTRAINT `key_list_ibfk_1` FOREIGN KEY (`r_id`) REFERENCES `requests` (`r_id`);
-
---
 -- Constraints for table `requests`
 --
 ALTER TABLE `requests`
   ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`u_id`) REFERENCES `accounts` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`admin_u_id`) REFERENCES `accounts` (`u_id`),
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`admin_u_id`) REFERENCES `accounts` (`u_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `requests_ibfk_3` FOREIGN KEY (`m_id`) REFERENCES `machines` (`m_id`);
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
